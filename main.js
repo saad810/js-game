21a820355762e313e086bd96ef6187f3716c6c19
saad810 <saadameen810@gmail.com>
2025-08-15 11:26:32 +0500

    update rendering

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
+import { resource } from './src/Resource';
+import { Sprite } from './src/Sprite';
+import { Vector2 } from './src/Vector2';
 import './style.css'
+const canvas = document.querySelector('#game-canvas');
+const ctx = canvas.getContext('2d');
+const sky = new Sprite({
+    resource: resource.images.sky,
+    frameSize: new Vector2(320, 180),
+})
+const ground = new Sprite({
+    resource: resource.images.ground,
+    frameSize: new Vector2(320, 180),
+})
+const shadow = new Sprite({
+    resource: resource.images.shadow,
+    frameSize: new Vector2(32, 32),
+})
+const hero = new Sprite({
+    resource: resource.images.hero,
+    frameSize: new Vector2(32, 32),
+    hFrame: 3,
+    vFrame: 8,
+    frame: 1,
+})
+const heroPos = new Vector2(16 * 6, 16 * 5);
+const heroOffset = new Vector2(-8, -22);
+const heroPosOffsetX = heroPos.x + heroOffset.x;
+const heroPosOffsetY = heroPos.y + heroOffset.y;
+
+const draw = () => {
+    sky.drawImage(ctx, 0, 0);
+    ground.drawImage(ctx, 0, 0);
+    shadow.drawImage(ctx, heroPosOffsetX, heroPosOffsetY);
+    hero.drawImage(ctx, heroPosOffsetX, heroPosOffsetY);
+
+}
+setInterval(() => {
+    console.log("Drawing frame");
+    draw();
+}, 300);
---
+++ src/Sprite.js
@@ -0,0 +1,70 @@
+import { Vector2 } from "./Vector2";
+
+export class Sprite {
+    constructor({
+        resource, //the image path or url
+        frameSize, //
+        hFrame,
+        vFrame,
+        frame,
+        scale,
+        position,
+
+    }) {
+        this.resource = resource; //the image path or url
+        this.frameSize = frameSize ?? new Vector2(16, 16);
+        this.hFrame = hFrame ?? 1;
+        this.vFrame = vFrame ?? 1;
+        this.frame = frame ?? 0;
+        this.scale = scale ?? 1;
+        this.position = position ?? new Vector2(0, 0);
+        this.frameMap = new Map();
+        this.buildFrameMap();
+    }
+
+    buildFrameMap() {
+        let frameCount = 0;
+        // iterate through vertical and horizontally on sprite sheet
+        for (let vertical = 0; vertical < this.vFrame; vertical++) {
+            for (let horizontal = 0; horizontal < this.hFrame; horizontal++) {
+                // console.log(`Adding frame ${frameCount} at position (${horizontal}, ${vertical})`);
+                this.frameMap.set(
+                    frameCount,
+                    new Vector2(
+                        this.frameSize.x * horizontal,
+                        this.frameSize.y * vertical
+                    )
+
+                )
+            }
+        }
+    }
+    drawImage(ctx, x, y) {
+        if (!this.resource.isLoaded) {
+            return;
+        }
+
+        // find correct sprite sheet frame
+        let frameCoordX = 0;
+        let frameCoordY = 0;
+        const frame = this.frameMap.get(this.frame);
+        if (frame) {
+            frameCoordX = frame.x;
+            frameCoordY = frame.y;
+        }
+        const frameSizeX = this.frameSize.x;
+        const frameSizeY = this.frameSize.y;
+
+        ctx.drawImage(
+            this.resource.image,
+            frameCoordX, // source x
+            frameCoordY, // source y
+            frameSizeX, // source width
+            frameSizeY, // source height
+            x, // destination/where to place on canvas x
+            y, // destination/where to place on canvas y
+            frameSizeX * this.scale, // destination width
+            frameSizeY * this.scale // destination height
+        );
+    }
+}
